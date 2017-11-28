fa1425de4d23d6aed86b13d932c9c6b10289ab99
Todd Matthews <gtmatthews@gmail.com>
2017-11-27 19:21:33 -0500

    Add instructor solution to bubblesort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,14 +1,31 @@
-function bubbleSort (nums) {
-  var i = 0;
-  while (nums[i] > nums[i +1]) {
-    for (var k = 0; k < nums.length; k++) {
-      if (nums[k] > nums[k+1]) {
-        var temp = nums[k];
-        nums[k] = nums[k+1];
-        nums[k+1] = temp; 
-        i = 0;
-        k = 0;
+// my first try
+// function bubbleSort (nums) {
+//   var i = 0;
+//   while (nums[i] > nums[i +1]) {
+//     for (var k = 0; k < nums.length; k++) {
+//       if (nums[k] > nums[k+1]) {
+//         var temp = nums[k];
+//         nums[k] = nums[k+1];
+//         nums[k+1] = temp; 
+//         i = 0;
+//         k = 0;
+//       }
+//     }
+//   }
+// } 
+
+// Brian Holt's Solution
+var bubbleSort = nums => {
+  do {
+    var swapped = false;
+    for (var i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i+1]) {
+        var temp = nums[i];
+        nums[i] = nums[i+1];
+        nums[i+1] = temp;
+        swapped = true;
       }
     }
-  }
-} 
+  } while (swapped); 
+};
+
